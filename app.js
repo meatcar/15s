c7ffbe87db74d2b32e7b9e808d75b7b1d9f8b2cb
Denys Pavlov <denys@viafoura.com>
2013-02-02 11:26:30 -0500

    Switched from jade to handlebars as a rendering

--- app.js
+++ app.js
@@ -6,14 +6,21 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
+  , hbs = require('express3-handlebars')
   , path = require('path');
 
 var app = express();
 
+app.engine('html', hbs({
+  defaultLayout: 'main',
+  extname: '.html'
+}));
+
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.set('view engine', 'html');
+  app.set("view options", { layout: false });
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
